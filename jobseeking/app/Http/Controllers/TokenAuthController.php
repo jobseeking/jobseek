2572aba8c6f2779711f51541f98b178118b3f9e1
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-03-30 20:46:44 +1100

    few

--- jobseeking/app/Http/Controllers/TokenAuthController.php
+++ jobseeking/app/Http/Controllers/TokenAuthController.php
@@ -71,7 +71,26 @@ class TokenAuthController extends Controller
 
     public function test(Request $request){
         
-        return redirect("./");
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+ 
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+ 
+            return response()->json(['token_expired'], $e->getStatusCode());
+ 
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+ 
+            return response()->json(['token_invalid'], $e->getStatusCode());
+ 
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+ 
+            return response()->json(['token_absent'], $e->getStatusCode());
+ 
+        }
+ 
+        return response()->json(compact('user'));
     }
 
 }
