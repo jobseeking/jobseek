8b3811a15a584492f0002338f9dd6b052e67d204
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-03-31 00:53:04 +1100

    wdq

--- jobseeking/resources/views/home.blade.php
+++ jobseeking/resources/views/home.blade.php
@@ -2,25 +2,71 @@
 
 
 @section('content')
-	<script>
-
-		function check_login_callback(is_login){
-			console.log(" home : check_login_callback : " + is_login);
-			if(is_login){
-				
-			}else{
-				
-			}
-		} 
-
-
-	</script>
 
 
 
-    <div class="content">
-        <div class="title">Laravel 5 (blade)</div>
-    </div>
+<div >
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="images/11.jpg" class="percent100" alt="">
+	      <div class="carousel-caption">
+	      </div>
+	    </div>
+
+	    <div class="item">
+	      <img src="images/22.jpg" class="percent100" alt="">
+	      <div class="carousel-caption">
+	      </div>
+	    </div>
+
+	    <div class="item">
+	      <img src="images/33.jpg" class="percent100" alt="">
+	      <div class="carousel-caption">
+	      </div>
+	    </div>
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	
+</div>
+
+
+<script>
+
+	function check_login_callback(is_login){
+		console.log(" home : check_login_callback : " + is_login);
+		if(is_login){
+			
+		}else{
+			
+		}
+	} 
+
+
+</script>
+
+
+
+    
 
 
 	
