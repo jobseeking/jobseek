d5c0f2a5c0cb9d543f26c8b3198dde47de661a74
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-03-30 18:37:39 +1100

    home

---
+++ jobseeking/resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+
+@section('content')
+	<script>
+
+		function check_login_callback(is_login){
+			console.log(" home : check_login_callback : " + is_login);
+			if(is_login){
+				
+			}else{
+				
+			}
+		} 
+
+
+	</script>
+
+
+
+    <div class="content">
+        <div class="title">Laravel 5 (blade)</div>
+    </div>
+
+
+	
+@endsection
+
+
