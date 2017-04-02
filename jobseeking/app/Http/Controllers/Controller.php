18404fcb1793e6b475b735ed1a6895a4d91b2297
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-02 20:34:33 +1000

    fefw

--- jobseeking/app/Http/Controllers/Controller.php
+++ jobseeking/app/Http/Controllers/Controller.php
@@ -18,8 +18,7 @@ class Controller extends BaseController
     public function __construct() {
 
        //$base_url = "/jobseek/jobseeking/public";
-      
-       View::share ( 'base_url', $base_url );
+       //View::share ( 'base_url', $base_url );
     } 
 
     // Get user info from token provided in HTTP POST parameter "post_token"
