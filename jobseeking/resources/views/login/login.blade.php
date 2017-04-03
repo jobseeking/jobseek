01f787bfd3f017540a5bf6abcde86ab1fb91c55b
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-03 21:08:15 +1000

    efew

--- jobseeking/resources/views/login/login.blade.php
+++ jobseeking/resources/views/login/login.blade.php
@@ -83,32 +83,34 @@
 
 
 	<div class="post_form" id="login_div" >
-		<h1 class="center">Log in</h1>
-		<div class="form-horizontal">
-		  <div class="form-group">
-		    <label for="Email" class="col-sm-2 control-label">Email address</label>
-		    <div class="col-sm-10">
-		      <input type="text" class="form-control" id="Email" placeholder="Email address">
-		    </div>
-		  </div>
+		 <div class="well">
+			<h1 class="center">Log in</h1>
+			<div class="form-horizontal">
+			  <div class="form-group">
+			    <label for="Email" class="col-sm-2 control-label">Email address</label>
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" id="Email" placeholder="Email address">
+			    </div>
+			  </div>
 
-		  <div class="form-group">
-		    <label for="Password" class="col-sm-2 control-label">Password</label>
-		    <div class="col-sm-10">
-		      <input type="password" class="form-control" id="Password" placeholder="Password">
-		    </div>
-		  </div>
-		  
-		  <div class="form-group">
-		    <div class="col-sm-offset-2 col-sm-10">
-		      <button type="submit" class="btn btn-default" onclick="onClickSubmit()">Log in</button>
-		    </div>
-		  </div>
-		</div>
+			  <div class="form-group">
+			    <label for="Password" class="col-sm-2 control-label">Password</label>
+			    <div class="col-sm-10">
+			      <input type="password" class="form-control" id="Password" placeholder="Password">
+			    </div>
+			  </div>
+			  
+			  <div class="form-group">
+			    <div class="col-sm-offset-2 col-sm-10">
+			      <button type="submit" class="btn btn-default" onclick="onClickSubmit()">Log in</button>
+			    </div>
+			  </div>
+			</div>
 
-		<div class="row">
-			<div class="col-sm-offset-2 col-sm-10">
-				Don't have an account? <a href="{{$base_url}}/register">Register</a>
+			<div class="row">
+				<div class="col-sm-offset-2 col-sm-10">
+					Don't have an account? <a href="{{$base_url}}/register">Register</a>
+				</div>
 			</div>
 		</div>
 	</div>
