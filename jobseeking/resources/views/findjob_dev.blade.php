c73932855a6fea5f0142af3258baaf5fd22e9669
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-05 15:59:45 +1000

    few

--- jobseeking/resources/views/findjob_dev.blade.php
+++ jobseeking/resources/views/findjob_dev.blade.php
@@ -34,8 +34,8 @@
 				<td><input type="text" class="form-control" name="type_id" value="{{Request::input("type_id")}}"></td>
 				<td><input type="text" class="form-control" name="classification_id" value="{{Request::input("classification_id")}}"></td>
 				<td><input type="text" class="form-control" name="user_id" value="{{Request::input("user_id")}}"></td>
-			
-				
+				<td></td>
+				<td></td>
 				<td style="min-width: 6em;">@include('vendor.crud.single-page-templates.common.search-btn')</td>
 			</form>
 		</tr>
