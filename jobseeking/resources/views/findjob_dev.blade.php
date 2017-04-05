5dae2236b54ec597da32925a740d927695fbf429
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-05 19:30:30 +1000

    efw

--- jobseeking/resources/views/findjob_dev.blade.php
+++ jobseeking/resources/views/findjob_dev.blade.php
@@ -36,41 +36,27 @@
 				    &nbsp;&nbsp;&nbsp;&nbsp; From
 				    <select class="form-control" name="">
 				        @for ($i = 0; $i < 200001; $i+=10000)
-				            <option value ="{{$i}}" > ${{number_format($i)}} </option>
-				        @endfor  
-				        <!--        
-                        <option value ="0" > $0 </option>
-                        <option value ="10000" > $10,000 </option>
-                        <option value ="20000" > $20,000 </option>   
-                        <option value ="30000" > $30,000 </option>   
-                        <option value ="40000" > $40,000 </option>   
-                        <option value ="50000" > $50,000 </option>   
-                        <option value ="60000" > $60,000 </option>   
-                        <option value ="70000" > $70,000 </option>   
-                        <option value ="80000" > $80,000 </option>   
-                        <option value ="90000" > $90,000 </option>   
-                        <option value ="100000" >$100,000 </option>   
-                        <option value ="120000" >$120,000 </option>   
-                        <option value ="150000" >$150,000 </option> 
-                        <option value ="200000" >$200,000 </option> 
-                        -->
+				        	@if ( Request::input("salary_bottom") == $i)
+				        		<option value ="{{$i}}" selected> ${{number_format($i)}} </option>
+				        	@else
+				        		<option value ="{{$i}}" > ${{number_format($i)}} </option>
+				        	@endif
+				        @endfor  		       
             		</select>
             		&nbsp;&nbsp;&nbsp;&nbsp; To
             		<select class="form-control" name="">
-                        <option value ="10000" > $10,000 </option>
-                        <option value ="20000" > $20,000 </option>   
-                        <option value ="30000" > $30,000 </option>   
-                        <option value ="40000" > $40,000 </option>   
-                        <option value ="50000" > $50,000 </option>   
-                        <option value ="60000" > $60,000 </option>   
-                        <option value ="70000" > $70,000 </option>   
-                        <option value ="80000" > $80,000 </option>   
-                        <option value ="90000" > $90,000 </option>   
-                        <option value ="100000" >$100,000 </option>   
-                        <option value ="120000" >$120,000 </option>   
-                        <option value ="150000" >$150,000 </option> 
-                        <option value ="200000" >$200,000 </option> 
-                        <option value ="99999999999999" >$Max </option>      
+            		    @for ($i = 10000; $i < 200001; $i+=10000)
+            		    	@if ( Request::input("salary_top") == $i)
+            		    		<option value ="{{$i}}" selected> ${{number_format($i)}} </option>
+            		    	@else
+            		    		<option value ="{{$i}}" > ${{number_format($i)}} </option>
+            		    	@endif
+				        @endfor
+				        @if ( Request::input("salary_top") > 200000 )
+				        	<option value ="99999999999999" selected> $Max </option>      
+				        @else
+				        	<option value ="99999999999999" > $Max </option>      
+				        @endif
             		</select>
 
 				</td>
