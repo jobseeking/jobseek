e5c2230956f0170f0588e2b8b10790e2e495f1c8
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-09 20:30:04 +1000

    wdwq

--- jobseeking/resources/views/home_findjob.blade.php
+++ jobseeking/resources/views/home_findjob.blade.php
@@ -103,9 +103,9 @@
 
 	</table>
 
-    <div class="center">
+    <div class="center" style="width:40%; height:10%;">
 		<button style="width:100%; height:100%;" class="btn btn-primary" onclick="onClickSubmit()">Seeking</button>
-	</div>>
+	</div>
 
 </div>
 
