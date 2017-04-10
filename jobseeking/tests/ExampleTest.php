55329e4e97403675f4d1ac12454f7519aa8bf35c
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-10 22:41:18 +1000

    eww

--- jobseeking/tests/ExampleTest.php
+++ jobseeking/tests/ExampleTest.php
@@ -28,7 +28,7 @@ class ExampleTest extends TestCase
         $this->visit('/login')
              ->type('rrr@rrr.rrr', 'Email')
              ->type('123456', 'Password')
-             ->click('login_btn')
+             ->findByNameOrId('#login_btn')->click()
              ->seePageIs('/');
 
         /*             
