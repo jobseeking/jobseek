cd93a7321ec446d1cc3a902ad5fe5616215daa23
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-22 12:22:32 +1000

    wdwd

--- jobseeking/resources/views/findjob.blade.php
+++ jobseeking/resources/views/findjob.blade.php
@@ -45,7 +45,7 @@
 
 </style>
 
-<h2>Find Job</h2>
+
 
 @forelse ( $records_suggested as $record )
 	{{  $record->id }}
@@ -54,6 +54,118 @@
 
 @endforelse
 
+<h2>Jobs you might be interested...</h2>
+
+<div class="table-responsive">  
+	<table class="table table-striped grid-view-tbl">
+	    
+	    <tbody>
+	    	@forelse ( $records_suggested as $record )
+		    	<tr>
+					<td>
+						{{ $record->id }}
+						</td>
+					<td>
+						<span 
+							  data-type="text"
+							  data-name="name"
+							  data-value="{{ $record->name }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->name }}</span>
+						</td>
+					<td>
+						<span 
+							  data-type="text"
+							  data-name="company"
+							  data-value="{{ $record->company }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->company }}</span>
+						</td>
+					<td>
+						<span 
+							  data-type="number"
+							  data-name="salary"
+							  data-value="{{ $record->salary }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->salary }}</span>
+						</td>
+					<td>
+						<span 
+							  data-type="text"
+							  data-name="details"
+							  data-value="{{ $record->details }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  ><div id="des_div" >{{ $record->details }}</div></span>
+						</td>
+					<td>
+						<span 
+							  data-type="number"
+							  data-name="location_id"
+							  data-value="{{ $record->location_id }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->location_name }}</span>
+						</td>
+					<td>
+						<span 
+							  data-type="number"
+							  data-name="type_id"
+							  data-value="{{ $record->type_id }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->type_name }}</span>
+						</td>
+					<td>
+						<span 
+							  data-type="number"
+							  data-name="classification_id"
+							  data-value="{{ $record->classification_id }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->classification_name }}</span>
+						</td>
+					<td>
+						<span 
+							  data-type="number"
+							  data-name="user_id"
+							  data-value="{{ $record->user_id }}"
+							  data-pk="{{ $record->{$record->getKeyName()} }}"
+							  data-url="{{$base_url}}/job/{{ $record->{$record->getKeyName()} }}"
+							  >{{ $record->user_name }}</span>
+						</td>
+					<td>
+						{{ $record->created_at }}
+						</td>
+					
+					<td class="actions-cell">
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						    <a href="{{$base_url.'/showjob'}}/{{$record->id}}"><i class="fa fa-eye fa-3x"></i></a>						
+					</td>
+
+		    	</tr>
+			@empty
+				@include ('vendor.crud.single-page-templates.common.not-found-tr',['colspan' => 12])
+	    	@endforelse
+	    </tbody>
+
+	</table>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<h2>Find Job</h2>
 
 <div class="table-responsive">  
 	<table class="table table-striped grid-view-tbl">
