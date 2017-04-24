52ffc621a3d02bea122d2147a04bc781336bf0be
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-04-24 22:25:48 +1000

    wdqw

--- jobseeking/tests/ExampleTest.php
+++ jobseeking/tests/ExampleTest.php
@@ -18,15 +18,15 @@ class ExampleTest extends TestCase
              ->see('Home');
         */
 
-        /*        
+        // test find job        
         $this->visit('/findjob')
              ->type('hhh', 'name')
              ->press('search')
              ->see('hhh');
              //->seePageIs('/findjob?classification_id=&company=&details=&id=&location_id=&name=hhh&salary_bottom=0&salary_top=99999999999999&type_id=&user_name=');
-        */
+        
 
-
+        // test post job, if response redirect to "showjob" page then success.
         $response = $this->call('POST', '/api/postjob', ['name' => 'unittest_job_name',
                                                         'company' => 'unittest_company',
                                                         'salary' => '20000',
@@ -41,14 +41,14 @@ class ExampleTest extends TestCase
         
 
 
-        /*             
+       // test login, if response contains auth token then login success.                    
        $this->json('POST', '/api/authenticate', ['email' => 'rrr@rrr.rrr', 'password' => '123456'])
             ->seeJsonStructure([
                  'token'                 
              ]);
-        */
+        
 
-        /*     
+        // test register, if response contains "name" then register success.     
         $this->json('POST', '/api/register', ['email' => rand().'@gmail.com',
                                               'password' => '123456',
                                               'name' => 'unittest_name',
@@ -56,7 +56,7 @@ class ExampleTest extends TestCase
              ->seeJsonStructure([
                  'name'                 
              ]);
-        */
+        
 
     }
 }
