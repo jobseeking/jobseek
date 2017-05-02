bb19fb4ed350754d8370676046b4423decd6722d
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-05-02 16:49:36 +1000

    few

--- jobseeking/resources/views/aboutus.blade.php
+++ jobseeking/resources/views/aboutus.blade.php
@@ -10,7 +10,9 @@
 	<h1 class='center'>About us</h1>
 	<h4>
 		Jobseeking is a diverse online business that aim to help people to find fulfilling and productive work and help organisations succeed. We have developed a great product technology solution in order for job seekers and hirers to facilitate job matches. Our extensive matchmaking entails asking our clients vital information to create the best possible match.
-
+	</h4>
+    <br>
+	<h4>
 		Jobseeking’s commitment to having a positive impact on society is reflected through the comprehensive range of initiatives which our businesses across the globe partake in to support the communities they operate within.
 		Over the 5 years of operation, Jobseeking continues to innovate within the markets. We provide a culture of excellence and acceptance in our workplaces and celebrate the diversity of employees that contribute to the success of our organisation.
 	</h4>
