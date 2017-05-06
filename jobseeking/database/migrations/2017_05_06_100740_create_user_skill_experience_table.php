0e3f78b722ed4951523dbdf0cb22aa5a5b5efc76
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-05-06 20:17:27 +1000

    wdqw

--- jobseeking/database/migrations/2017_05_06_100740_create_user_skill_experience_table.php
+++ jobseeking/database/migrations/2017_05_06_100740_create_user_skill_experience_table.php
@@ -12,7 +12,21 @@ class CreateUserSkillExperienceTable extends Migration
      */
     public function up()
     {
-        //
+        if (!Schema::hasTable('user_skill_experience')){
+            Schema::create('user_skill_experience', function (Blueprint $table) {
+                $table->increments('id');
+               
+                $table->integer('user_id')->unsigned();
+                $table->foreign('user_id')->references('id')->on('users');
+
+                $table->integer('skill_id')->unsigned();
+                $table->foreign('skill_id')->references('id')->on('classification_skills');
+
+                $table->integer('experience_years')->unsigned()->default(0);
+
+                $table->timestamps();
+            });
+        }
     }
 
     /**
--- jobseeking/database/migrations/2017_05_06_100758_create_job_skill_experience_table.php
+++ jobseeking/database/migrations/2017_05_06_100758_create_job_skill_experience_table.php
@@ -12,7 +12,21 @@ class CreateJobSkillExperienceTable extends Migration
      */
     public function up()
     {
-        //
+        if (!Schema::hasTable('job_skill_experience')){
+            Schema::create('job_skill_experience', function (Blueprint $table) {
+                $table->increments('id');
+               
+                $table->integer('job_id')->unsigned();
+                $table->foreign('job_id')->references('id')->on('jobs');
+
+                $table->integer('skill_id')->unsigned();
+                $table->foreign('skill_id')->references('id')->on('classification_skills');
+
+                $table->integer('experience_years')->unsigned()->default(0);
+
+                $table->timestamps();
+            });
+        }
     }
 
     /**
