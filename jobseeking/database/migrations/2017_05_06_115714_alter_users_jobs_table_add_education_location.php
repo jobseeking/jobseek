6e33d59900f515982ac3b5dbe38d6a04e79d1c63
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-05-06 22:12:21 +1000

    21e1

--- jobseeking/database/migrations/2017_05_06_115714_alter_users_jobs_table_add_education_location.php
+++ jobseeking/database/migrations/2017_05_06_115714_alter_users_jobs_table_add_education_location.php
@@ -12,11 +12,11 @@ class AlterUsersJobsTableAddEducationLocation extends Migration
      */
     public function up()
     {
-        /*
+        
         if (!Schema::hasColumn('users', 'location_id')) {
             Schema::table('users', function(Blueprint $table)
             {
-                $table->integer('location_id')->unsigned();
+                $table->integer('location_id')->unsigned()->default(1);
                 $table->foreign('location_id')->references('id')->on('locations');
             });
         }
@@ -24,16 +24,16 @@ class AlterUsersJobsTableAddEducationLocation extends Migration
         if (!Schema::hasColumn('users', 'education_id')) {
             Schema::table('users', function(Blueprint $table)
             {
-                $table->integer('education_id')->unsigned();
+                $table->integer('education_id')->unsigned()->default(1);
                 $table->foreign('education_id')->references('id')->on('education');
             });
         }
-*/
-        
+
+
         if (!Schema::hasColumn('jobs', 'education_id')) {
             Schema::table('jobs', function(Blueprint $table)
             {
-                $table->integer('education_id')->unsigned();
+                $table->integer('education_id')->unsigned()->default(1);
                 $table->foreign('education_id')->references('id')->on('education');
             });
         }
