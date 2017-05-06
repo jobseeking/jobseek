baec221a54d9a71fabc35194f33f289f7dd4ded3
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-05-06 21:38:39 +1000

    dwq

--- jobseeking/resources/views/postjob.blade.php
+++ jobseeking/resources/views/postjob.blade.php
@@ -120,6 +120,31 @@
             </select>
           </div>
 
+          @foreach ( $classifications as $classification )
+              @if ( $classification->id == 1)
+              <div class="col-sm-5 skills_display" 
+                   id="classification_{{$classification->id}}_skill" >
+              @else
+              <div class="col-sm-5" 
+                   id="classification_{{$classification->id}}_skill" style="display:none" >
+              @endif
+
+              @foreach ( $classification_skills as $classification_skill )
+                  @if ( $classification->id == 
+                        $classification_skill->classification_id)
+                  <select class="form-control" name="classification_skill_{{$classification_skill->id}}">
+                    <option value ="0" > {{$classification_skill->name}} No Experience </option>
+                    <option value ="1" > {{$classification_skill->name}} < 1 year </option>
+                    <option value ="3" > {{$classification_skill->name}} 1 ~ 3 years </option>
+                    <option value ="5" > {{$classification_skill->name}} 3 ~ 5 years </option>
+                    <option value ="10"> {{$classification_skill->name}} > 5 years </option>
+                </select>
+                  @endif
+              @endforeach  
+              </div> 
+          @endforeach   
+
+<!--
           <div class="col-sm-5" id = "skill_experience_1">
             <select class="form-control" >
                 <option value ="" > Java No Experience </option>
@@ -143,29 +168,7 @@
                 <option value ="" > PHP > 5 years </option>
             </select>
           </div>
-          <div class="col-sm-5" id = "skill_experience_2" style="display:none" >
-            <select class="form-control" >
-                <option value ="" > Audit No Experience </option>
-                <option value ="" > Audit < 1 year </option>
-                <option value ="" > Audit 1 ~ 3 years </option>
-                <option value ="" > Audit 3 ~ 5 years </option>
-                <option value ="" > Audit > 5 years </option>
-            </select>
-            <select class="form-control" >
-                <option value ="" > Payroll No Experience </option>
-                <option value ="" > Payroll < 1 year </option>
-                <option value ="" > Payroll 1 ~ 3 years </option>
-                <option value ="" > Payroll 3 ~ 5 years </option>
-                <option value ="" > Payroll > 5 years </option>
-            </select>
-            <select class="form-control" >
-                <option value ="" > Tax No Experience </option>
-                <option value ="" > Tax < 1 year </option>
-                <option value ="" > Tax 1 ~ 3 years </option>
-                <option value ="" > Tax 3 ~ 5 years </option>
-                <option value ="" > Tax > 5 years </option>
-            </select>
-          </div>
+-->     
 
         </div>
 
